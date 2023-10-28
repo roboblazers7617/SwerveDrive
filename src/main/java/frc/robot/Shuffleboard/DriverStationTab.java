c9af4ba9389cdf15177efe527c9536ca1a600d81
fodfodfod <94200657+fodfodfod@users.noreply.github.com>
2023-10-28 16:46:49 -0400

    use updated shuffleboardLib

--- src/main/java/frc/robot/Shuffleboard/DriverStationTab.java
+++ src/main/java/frc/robot/Shuffleboard/DriverStationTab.java
@@ -13,23 +13,33 @@ import shuffleboardlib.Questionnaire;
 /** Add your docs here. */
 public class DriverStationTab extends ShuffleboardTabBase {
     private final SwerveDrive swerveDrive;
+    private Questionnaire questionnaire;
+
+
     public DriverStationTab(SwerveDrive swerveDrive){
         this.swerveDrive = swerveDrive;
 
-        HashMap<String, Question> pieceNumber = new HashMap<>();
-        pieceNumber.put("1", new Question("1 piece", null));
-        pieceNumber.put("2", new Question("2 piece", null));
-        Question pieceNumberQuestion = new Question("How many pieces?", pieceNumber);
+        HashMap<String, Question> leftPieceNumber = new HashMap<>();
+        leftPieceNumber.put("1", new Question("1 piece", null, true, "leftOne"));
+        leftPieceNumber.put("2", new Question("2 piece", null, true, "leftTwo"));
+        Question leftPieceNumberQuestion = new Question("How many pieces?", leftPieceNumber, false, null);
+
+        HashMap<String, Question> rightPieceNumber = new HashMap<>();
+        rightPieceNumber.put("1", new Question("1 piece", null, true, "rightOne"));
+        rightPieceNumber.put("2", new Question("2 piece", null, true, "rightTwo"));
+        Question rightPieceNumberQuestion = new Question("How many pieces?", leftPieceNumber, false, null);
 
         HashMap<String, Question> answersHashMap = new HashMap<>();
-        answersHashMap.put("left", pieceNumberQuestion);
-        answersHashMap.put("middle", new Question("Do you like sushi?", null));
-        answersHashMap.put("right", pieceNumberQuestion);
+        answersHashMap.put("left", leftPieceNumberQuestion);
+        answersHashMap.put("middle", new Question ("middle", null, true, "middleCone"));
+        answersHashMap.put("right", rightPieceNumberQuestion);
+        Question rootQuestion = new Question("starting position", answersHashMap, false, null);
 
-        new Questionnaire("Driver Station", new Question("starting position", answersHashMap), 3);
+        questionnaire = new Questionnaire("Driver Station", rootQuestion, 5);
+
 
     }
-    
+
     @Override
     public void update() {
     }
